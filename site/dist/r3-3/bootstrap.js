5a50debb1779862cf097fee987d7366f634605cd
Haihao_Nature Grace HK <haihao0307@gmail.com>
2026-09-10 04:08:06 +0800

    feat(wenzhou): add ordered R3.3 bootstrap

---
+++ site/dist/r3-3/bootstrap.js
@@ -0,0 +1,5 @@
+import {installSurfaceEvidence} from './surface-evidence.js';
+
+installSurfaceEvidence();
+await import('../r3-2/bootstrap.js');
+document.documentElement.dataset.wenzhouR33Boot='true';
