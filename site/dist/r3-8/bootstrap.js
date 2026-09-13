2ad4ff7712f009b234d1786169c8226624d796b5
Haihao_Nature Grace HK <haihao0307@gmail.com>
2026-09-13 15:46:29 +0800

    feat(wenzhou): install WRB JRC gzip transport before environment context

--- site/dist/r3-8/bootstrap.js
+++ site/dist/r3-8/bootstrap.js
@@ -1,11 +1,13 @@
 import * as THREE from 'three';
 import {installOverlayTransformContract} from './overlay-transform-contract.js';
 import {installSoilPairLoader} from './soil-pair-loader.js';
+import {installEvidenceGzipLoader} from './evidence-gzip-loader.js';
 import {installSoilContext} from './soil-context.js';
 import {installEnvironmentContext} from './environment-context.js';
 import {installWorldScore} from './world-score.js';
 installOverlayTransformContract(THREE);
 installSoilPairLoader();
+installEvidenceGzipLoader();
 THREE.Object3D.prototype[Symbol.for('wenzhou.r3.3.surface-evidence-installed')]=true;
 installSoilContext();installEnvironmentContext();installWorldScore();
 await import('../r3-6/bootstrap.js');
