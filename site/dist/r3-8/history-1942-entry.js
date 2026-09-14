9319921487c40d62f1b72e790e9751770ebbb9cd
Haihao_Nature Grace HK <haihao0307@gmail.com>
2026-09-14 17:52:21 +0800

    feat(wenzhou): wire 1953 control layer into historical workspace

--- site/dist/r3-8/history-1942-entry.js
+++ site/dist/r3-8/history-1942-entry.js
@@ -1,2 +1,4 @@
 import {installHistory1942Preclean} from './history-1942-preclean.js';
+import {installHistorical1953ControlLayer} from './history-1953-control-layer.js';
 installHistory1942Preclean();
+installHistorical1953ControlLayer();
