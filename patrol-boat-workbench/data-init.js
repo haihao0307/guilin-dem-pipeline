14cedc9a1188219b2bf0d3df210f2dfec31bbacf
Haihao_Nature Grace HK <haihao0307@gmail.com>
2026-09-23 15:37:01 +0800

    patrol boat workbench: add data metadata

---
+++ patrol-boat-workbench/data-init.js
@@ -0,0 +1,3 @@
+window.PATROL_BOAT_DATA = {"size":[4.81003285147176,1.2587786965345131,1.419031492940377],"groups":{"hull":{"vertexCount":2693,"indexCount":9489},"engine":{"vertexCount":4907,"indexCount":18975}}};
+window.PATROL_BOAT_DATA.hull = Object.assign(window.PATROL_BOAT_DATA.groups.hull, {});
+window.PATROL_BOAT_DATA.engine = Object.assign(window.PATROL_BOAT_DATA.groups.engine, {});
